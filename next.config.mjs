dbda3e7fba1b32998da4b3d21795131be14ff6b5
Tungduong219 <dotungduong2194@gmail.com>
2026-08-30 14:57:39 +0700

    fix(security): add Cross-Origin-Opener-Policy same-origin-allow-popups for smooth Google Auth popup

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,19 @@
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
